4a55e071acdd3a376c27421b562295877459fe5f
Ville Lukka <ville@qem.fi>
2022-02-16 18:01:09 +0200

    Checking env variables.

--- src/hooks.js
+++ src/hooks.js
@@ -1,29 +1,36 @@
-import cookie from 'cookie';
-import { v4 as uuid } from '@lukeed/uuid';
+// import cookie from 'cookie';
+// import { v4 as uuid } from '@lukeed/uuid';
 
-export const handle = async ({ event, resolve }) => {
-	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
-	event.locals.userid = cookies.userid || uuid();
-
-	const response = await resolve(event);
-
-	if (!cookies.userid) {
-		// if this is the first time the user has visited this app,
-		// set a cookie so that we recognise them when they return
-		response.headers.set(
-			'set-cookie',
-			cookie.serialize('userid', event.locals.userid, {
-				path: '/',
-				httpOnly: true
-			})
-		);
-	}
-
-	return response;
-};
+// export const handle = async ({ event, resolve }) => {
+// 	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
+// 	event.locals.userid = cookies.userid || uuid();
+//
+// 	const response = await resolve(event);
+//
+// 	if (!cookies.userid) {
+// 		// if this is the first time the user has visited this app,
+// 		// set a cookie so that we recognise them when they return
+// 		response.headers.set(
+// 			'set-cookie',
+// 			cookie.serialize('userid', event.locals.userid, {
+// 				path: '/',
+// 				httpOnly: true
+// 			})
+// 		);
+// 	}
+//
+// 	return response;
+// };
 
 export function getSession() {
+	let beebeesApi = '';
+	if (process && process.env) {
+		beebeesApi = process.env['BEEBEES_API'];
+	}
+	else {
+		beebeesApi = import.meta.env.BEEBEES_API;
+	}
 	return {
-		BEEBEES_API: import.meta.env.BEEBEES_API// || 'https://beebees-dev.vilvei.workers.dev/api'
+		BEEBEES_API: beebeesApi // || 'https://beebees-dev.vilvei.workers.dev/api'
 	};
 }
