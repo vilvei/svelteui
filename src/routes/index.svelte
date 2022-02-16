06d157c6f3a57199d1d611adb914122d2b7cb9fb
Ville Lukka <ville@qem.fi>
2022-02-16 14:45:28 +0200

    Testing svelte+tailwind+daisyui+cloudlare-pages.

---
+++ src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+	<div>to your new SvelteKit app</div>
+	<button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-yellow-400 transition duration-200 ease-in-out hover:bg-gray-900">Button</button>
+
+<style>
+
+	div {
+		@apply text-4xl bg-gray-800 text-yellow-400 py-2 px-6 font-bold;
+	}
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+
+module.exports = {
+  content: ['./src/**/*.svelte', './src/**/*.css'],
+  darkMode: 'media',
+  variants: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+}
